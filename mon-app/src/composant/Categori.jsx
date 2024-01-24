736fc41bbe26611ee61c2ea09f37a581d35bd51c
harry92300 <133029394+harry92300@users.noreply.github.com>
2024-01-24 15:02:49 +0100

    RECHERCHER LES LIVRES POUR LE SITE WEB

--- mon-app/src/composant/Categori.jsx
+++ mon-app/src/composant/Categori.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/Categori.css';
 import Carrousel1 from '../image/carrousel1.jpg';
 import Devello from '../image/Captive-tome-1.jpg';
 import She from '../image/sheHULG.jpeg';
-import Carrou from '../image/Devellopemntpersonnel.jpg'
-import Chat from '../image/leChatDuBois.jpeg'
-import Messi from '../image/Messi.jpeg'
-import Reve from '../image/Reve.jpeg'
-import Baki from '../image/Baki.jpeg'
-import Cedric from '../image/Cedric.jpeg'
-import Nancy from '../image/Nancy.jpeg'
-import Charlie from '../image/Charlie.jpeg'
-import Haiti from '../image/Haiti.jpeg'
-import Espace from '../image/Espace.jpeg'
-
+import Carrou from '../image/Devellopemntpersonnel.jpg';
+import Chat from '../image/leChatDuBois.jpeg';
+import Messi from '../image/Messi.jpeg';
+import Reve from '../image/Reve.jpeg';
+import Baki from '../image/Baki.jpeg';
+import Cedric from '../image/Cedric.jpeg';
+import Nancy from '../image/Nancy.jpeg';
+import Charlie from '../image/Charlie.jpeg';
+import Haiti from '../image/Haiti.jpeg';
+import Espace from '../image/Espace.jpeg';
 
 
 
 const Categori = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  
+const books = [
+  { title: 'Fiction pour jeunes adultes', author: 'Sarah Rivens', price: 12, image: Devello },
+  { title: 'Comics, Super-héros', author: 'Stan Lee et Juan Bobillo', price: 12, image: She },
+  { title: 'Sciences humaines', author: 'Robert Mercier', price: 12, image: Carrou },
+  { title: 'Poésie', author: 'Marie Boulic', price: 12, image: Chat },
+  { title: 'Sports', author: 'Guillem Balagué', price: 12, image: Messi },
+  { title: 'Arts décoratifs', author: 'Sibylline Meynet', price: 12, image: Reve },
+  { title: 'Mangas', author: 'Keisuke Itagaki', price: 12, image: Baki },
+  { title: 'BDs humoristiques', author: 'Laudec et Raoul Cauvin', price: 12, image: Cedric },
+  { title: 'Littérature anglo-saxonne', author: 'Nancy Tucker', price: 12, image: Nancy },
+  { title: 'Loisirs', author: 'Martin Handford', price: 1, image: Charlie },
+  { title: 'Histoire littéraire', author: 'Anthony Sr Kavanagh', price: 12, image: Haiti },
+  { title: 'Science-fiction', author: 'Colin Greenland', price: 12, image: Espace },
+ 
+];
+
+  
+
+  
   return (
     <div className="categories-container">
       <div className='image1'>
@@ -27,17 +48,21 @@ const Categori = () => {
       <div className='exposition'>
         <h2 className='mesCategori'>Nos Livres</h2>
       </div>
-
-
-
-
-
-
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Rechercher un livre..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
 
 
   
       <div className='contien'>
+        
                 <div className="image-wrapper">
+                  
                     <img src={Devello} alt="Livre 1" className="small-image" />
                     <button class="favorite styled" type="button">Fiction pour jeunes adultes</button>
                     <p>Sarah Rivens(Auteur) </p>
@@ -139,7 +164,7 @@ const Categori = () => {
         {/* Intégrez ici votre carte Google Maps */}
         <iframe
             title="Google Maps"
-            src="https://maps.app.goo.gl/eYsTWth6AFNtWYfj6"
+            src="https://maps.app.goo.gl/twhJ37eWVy8E1fsj6"
             width="400"
             height="300"
             allowFullScreen=""
