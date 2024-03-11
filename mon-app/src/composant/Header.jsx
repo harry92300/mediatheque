599b8fb88f4dc621caceda3813f95577aaff4490
harry92300 <133029394+harry92300@users.noreply.github.com>
2024-03-11 14:08:58 +0100

    MISE A JOUR DE LA NAVEBAR PLUS LES LIVRES

--- mon-app/src/composant/Header.jsx
+++ mon-app/src/composant/Header.jsx
@@ -8,16 +8,19 @@ export default function Header() {
   return (
     <header className="header">
         <div className="logo">
-            <video width="100" height="100" controls>
+            <video width="50" height="50" controls>
                 <source src={Logo} type="video/mp4" />
             </video>
         </div>
         <nav className="nav">
             <ul>
-                <li><Link to="/inscription">Inscription</Link></li>
-                <li><a href="/Connexion">Connexion</a></li>
-                <li><a href="/Catégorie de Livre">Catégorie de Livre</a></li>
                 <li><a href="/">Bienvenue</a></li>
+                <li><a href="/Catégorie de Livre">Catégorie de Livre</a></li>
+                <li><a href="/Connexion">Connexion</a></li>
+                <li><Link to="/inscription">Inscription</Link></li>
+                
+                
+                
             </ul>
         </nav>
     </header>
